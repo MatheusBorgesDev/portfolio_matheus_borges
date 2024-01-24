c4f90cc00c5b7a4d996f500a3a78de949343f196
MatheusBorgesDev <matheusb.dev@gmail.com>
2024-01-24 11:11:58 -0300

    feat: create Header component

---
+++ src/components/ui/Header.tsx
@@ -0,0 +1,15 @@
+import { Card } from "./card";
+
+const Header = () => {
+  return (
+    <Card className="p-3">
+      <p className="font-bold text-xl">
+        {"</"}
+        <span className="text-cyan-700">MatheusBorges</span>
+        {">"}
+      </p>
+    </Card>
+  );
+};
+
+export default Header;
