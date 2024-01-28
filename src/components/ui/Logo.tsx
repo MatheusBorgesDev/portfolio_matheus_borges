da9aa8811fbb300eb4d5ecf20908aeff5cf10992
MatheusBorgesDev <matheusb.dev@gmail.com>
2024-01-28 16:11:26 -0300

    feat: add Logo component

---
+++ src/components/ui/Logo.tsx
@@ -0,0 +1,13 @@
+const Logo = () => {
+  return ( 
+    <div>
+      <h2 className="text-primary font-bold text-[2rem] w-full">
+          {"<"}
+          <span>MatheusBorges</span>
+          {"/>"}
+        </h2>
+    </div>
+   );
+}
+ 
+export default Logo;
