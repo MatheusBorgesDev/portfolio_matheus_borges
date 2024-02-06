af4ccd5de20bab5dfd80cb0c6409925958a4cd54
MatheusBorgesDev <matheusb.dev@gmail.com>
2024-02-06 17:36:51 -0300

    feat: add highlights projects session to home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,6 +1,13 @@
-import Header from "@/components/ui/Header";
-import TechnologyItem from "@/components/ui/TechnologyItem";
+import ProjectItem from "@/components/ui/ProjectItem";
+import TechnologiesField from "@/components/ui/TechnologiesField";
 import { Button } from "@/components/ui/button";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 import { Separator } from "@/components/ui/separator";
 import { ArrowUpRight } from "lucide-react";
 import Image from "next/image";
@@ -27,46 +34,66 @@ export default function Home() {
 
         <div>
           <h1 className="text-md font-bold opacity-65">
-            Olá, Matheus Borges aqui!
+            Olá, Matheus Borges aqui! &#128075;
           </h1>
           <h2 className="text-[3.5rem] text-primary leading-[3rem] font-bold">
-           Desenvolvedor front-end.
+            Desenvolvedor front-end.
           </h2>
         </div>
 
-        <div className="flex rounded-xl bg-primary-foreground gap-4 flex-wrap p-5 justify-between">
-          <TechnologyItem imgUrl="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" />
-          <TechnologyItem imgUrl="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg" />
-          <TechnologyItem imgUrl="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg" />
-          <TechnologyItem imgUrl="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg" />
-          <TechnologyItem imgUrl="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
-          <TechnologyItem imgUrl="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" />
-          <TechnologyItem imgUrl="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" />
-          <TechnologyItem imgUrl="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" />
-
-          <div className="flex w-full items-center justify-center">
-          <Button className="object-fit">
-            <div className="flex gap-2">
-              <p>Veja os projetos</p>
-              <ArrowUpRight />
-            </div>
-          </Button>
-        </div>
-        </div>
+        <TechnologiesField />
       </div>
 
       <Separator />
 
-      <div className="my-12">
+      <div className="flex flex-col gap-10 my-12 py-4">
         <div>
           <p className="text-md opacity-65 font-bold">
             {"<"}helloWorld{"/>"}
           </p>
-          <p className="text-[3.5rem] leading-[3rem] font-bold">Projetos</p>
+          <p className="text-[3.5rem] leading-[3rem] font-bold">
+            Projetos em destaque.
+          </p>
         </div>
 
-        
+        <div className="flex flex-col gap-4">
+          <Carousel className="bg-primary-foreground rounded-lg">
+            <CarouselContent className="-ml-4 cursor-grab active:cursor-grabbing">
+              <CarouselItem className="basis-3/3 pl-10">
+                <ProjectItem
+                  projectName="My Coffee Place"
+                  imageUrl="https://images.unsplash.com/photo-1609534655988-3f5225c13553?q=80&w=1970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                />
+              </CarouselItem>
+
+              <CarouselItem className="basis-3/3 pl-10">
+                <ProjectItem
+                  projectName="Keep Moving!"
+                  imageUrl="https://plus.unsplash.com/premium_photo-1674605365723-15e6749630f4?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                />
+              </CarouselItem>
+
+              <CarouselItem className="basis-3/3 pl-10">
+                <ProjectItem
+                  projectName="Take note"
+                  imageUrl="https://images.unsplash.com/photo-1620287920810-3f5b9746380c?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                />
+              </CarouselItem>
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+          </Carousel>
+
+          <Button className="object-fit">
+            <div className="flex gap-2 items-center">
+              <p>Acesse todos os projetos</p>
+              <ArrowUpRight />
+            </div>
+          </Button>
+        </div>
       </div>
+
+      <Separator />
     </div>
   );
 }
