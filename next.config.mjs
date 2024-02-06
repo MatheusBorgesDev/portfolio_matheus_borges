348a5048ea669afa04e7d2e505849647f3f6f664
MatheusBorgesDev <matheusb.dev@gmail.com>
2024-02-06 17:32:16 -0300

    test: add unsplash domain

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["github.com"],
+    domains: ["github.com", "images.unsplash.com", "plus.unsplash.com"],
   },
 };
 
