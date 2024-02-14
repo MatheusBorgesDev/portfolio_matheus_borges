41e258fbc7be5709cfd2c852d364ac336d04a4ab
MatheusBorgesDev <matheusb.dev@gmail.com>
2024-02-14 18:07:11 -0300

    style: background

--- src/components/ui/TechnologiesField.tsx
+++ src/components/ui/TechnologiesField.tsx
@@ -5,13 +5,23 @@ import { Button } from "./button";
 const TechnologiesField = () => {
   return (
     <>
-      <div className="flex rounded-xl bg-foreground border-primary backdrop-blur-sm gap-4 flex-wrap p-5 justify-between shadow-brutal">
-
-        <TechnologyItem imgUrl="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" techName="React">
-        </TechnologyItem>
-        <TechnologyItem imgUrl="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg" techName="Next.JS"/>
-        <TechnologyItem imgUrl="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg" techName="TypeScript"/>
-        <TechnologyItem imgUrl="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg" techName="Tailwind"/>
+      <div className="flex rounded-xl bg-transparent border-primary backdrop-blur-sm gap-4 flex-wrap p-5 justify-between shadow-brutal ">
+        <TechnologyItem
+          imgUrl="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"
+          techName="React"
+        ></TechnologyItem>
+        <TechnologyItem
+          imgUrl="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg"
+          techName="Next.JS"
+        />
+        <TechnologyItem
+          imgUrl="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg"
+          techName="TypeScript"
+        />
+        <TechnologyItem
+          imgUrl="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg"
+          techName="Tailwind"
+        />
 
         <div className="flex w-full items-center justify-center">
           <Button variant="secondary" className="text-foreground object-fit">
