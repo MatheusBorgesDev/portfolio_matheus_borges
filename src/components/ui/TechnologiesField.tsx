d24d849d7d6d33db8b1299bb550f21b37a27c8c3
MatheusBorgesDev <matheusb.dev@gmail.com>
2024-02-27 22:39:09 -0300

    style: background adjusts

--- src/components/ui/TechnologiesField.tsx
+++ src/components/ui/TechnologiesField.tsx
@@ -6,33 +6,35 @@ import Link from "next/link";
 const TechnologiesField = () => {
   return (
     <>
-      <div className="flex rounded-xl bg-background border-primary backdrop-blur-sm gap-4 flex-wrap p-5 justify-between shadow-brutal ">
-        <TechnologyItem
-          imgUrl="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"
-          techName="React"
-        ></TechnologyItem>
-        <TechnologyItem
-          imgUrl="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg"
-          techName="Next.JS"
-        />
-        <TechnologyItem
-          imgUrl="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg"
-          techName="TypeScript"
-        />
-        <TechnologyItem
-          imgUrl="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg"
-          techName="Tailwind"
-        />
+      <div className="bg-bgImg bg-contain">
+        <div className="flex rounded-xl border-primary backdrop-blur-md gap-4 flex-wrap p-5 justify-between shadow-brutal ">
+          <TechnologyItem
+            imgUrl="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"
+            techName="React"
+          ></TechnologyItem>
+          <TechnologyItem
+            imgUrl="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg"
+            techName="Next.JS"
+          />
+          <TechnologyItem
+            imgUrl="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg"
+            techName="TypeScript"
+          />
+          <TechnologyItem
+            imgUrl="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg"
+            techName="Tailwind"
+          />
 
-        <div className="flex w-full items-center justify-center">
-          <Button variant="secondary" className="text-foreground object-fit">
-            <Link href="/projects">
-              <div className="flex gap-2 items-center">
-                <p>Veja os projetos</p>
-                <ArrowUpRight />
-              </div>
-            </Link>
-          </Button>
+          <div className="flex w-full items-center justify-center">
+            <Button variant="secondary" className="text-foreground object-fit">
+              <Link href="/projects">
+                <div className="flex gap-2 items-center">
+                  <p>Veja os projetos</p>
+                  <ArrowUpRight />
+                </div>
+              </Link>
+            </Button>
+          </div>
         </div>
       </div>
     </>
