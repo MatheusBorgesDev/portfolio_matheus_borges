6eaa538dd81c52280cb77d3931b04fe01bb12ea7
MatheusBorgesDev <matheusb.dev@gmail.com>
2024-02-28 00:02:41 -0300

    feat: ensure project details page shows project by URL id params

--- src/app/projects/[id]/page.tsx
+++ src/app/projects/[id]/page.tsx
@@ -4,26 +4,26 @@ import { Button } from "@/components/ui/button";
 import { ArrowUpRightIcon, GithubIcon, GlobeIcon } from "lucide-react";
 import Image from "next/image";
 import BackButton from "./components/BackButton";
+import projectsData from "@/utils/projectsData";
+import Link from "next/link";
 
 const ProjectDetails = ({ params }: { params: { id: string } }) => {
   const id = params.id;
 
+  const project = projectsData.find((project) => project.id === id);
+
   const projectImg =
     "https://images.unsplash.com/photo-1609534655988-3f5225c13553?q=80&w=1970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
 
-  const projectDate = "Dez - 2023";
-
   const imgTechUrl =
     "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg";
-
-  const projectName = "My Coffe Place";
 
   return (
     <div className="flex flex-col px-4 gap-4 text-secondary py-20">
       <div className="flex flex-col gap-4">
         <div className="relative">
           <Image
-            src={projectImg}
+            src={`/projects-images/${project?.images[0]}`}
             width={0}
             height={0}
             className="h-[20rem] w-full object-cover rounded-lg"
@@ -39,8 +39,9 @@ const ProjectDetails = ({ params }: { params: { id: string } }) => {
 
       <div className="flex rounded-xl bg-transparent border-primary backdrop-blur-sm gap-4 flex-wrap p-5 justify-between shadow-brutal">
         <div className="flex w-full justify-between items-center">
-          {projectDate}
-
+          <h1 className="text-4xl text-primary leading-[3rem] font-bold">
+            {project?.name}
+          </h1>
           <div className="flex gap-2">
             <Image
               src={imgTechUrl}
@@ -80,30 +81,31 @@ const ProjectDetails = ({ params }: { params: { id: string } }) => {
           </div>
         </div>
 
-        <h1 className="text-4xl text-primary leading-[3rem] font-bold">
-          {projectName}
-        </h1>
+        <div className="flex flex-col gap-4">
+          <h2 className="font-bold text-lg">{project?.intro}</h2>
 
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi
-          repellendus similique mollitia expedita, enim praesentium aliquam
-          adipisci deleniti aperiam earum perspiciatis doloribus quas odit
-          nostrum aut, nobis cupiditate totam ipsa.
-        </p>
+          <p className="text-md opacity-80">{project?.description}</p>
+        </div>
 
         <div className="flex flex-col justify-between items-center gap-4 w-full">
-          <Button
-            variant="secondary"
-            className="flex justify-between w-full h-12"
-          >
-            <GlobeIcon /> Demo <ArrowUpRightIcon />{" "}
+          <Button variant="secondary" className="w-full h-12">
+            <Link
+              href={`${project?.demoLink}`}
+              className="flex justify-between w-full items-center"
+              target="_blank"
+            >
+              <GlobeIcon /> Demo <ArrowUpRightIcon />{" "}
+            </Link>
           </Button>
 
-          <Button
-            variant="secondary"
-            className="flex justify-between w-full h-12"
-          >
-            <GithubIcon /> Código <ArrowUpRightIcon />{" "}
+          <Button variant="secondary" className="w-full h-12">
+            <Link
+              href={`${project?.gitLink}`}
+              className="flex justify-between w-full items-center"
+              target="_blank"
+            >
+              <GithubIcon /> Código <ArrowUpRightIcon />{" "}
+            </Link>
           </Button>
         </div>
       </div>
